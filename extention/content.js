2e8aa287508011b4faf99bce51caa0aca6d180ce
Towaiji <aliyasiraltowaiji@gmail.com>
2025-07-25 14:09:54 +0400

    selectors

--- extention/content.js
+++ extention/content.js
@@ -4,7 +4,7 @@ window.startYelpScraper = async function() {
   for (let i = 0; i < businessCards.length; i++) {
     let card = businessCards[i];
     card.scrollIntoView({behavior: 'smooth'});
-    await new Promise(res => setTimeout(res, 900 + Math.random()*700));
+    await new Promise(res => setTimeout(res, 900 + Math.random() * 700));
 
     // Get business link
     let link = card.querySelector('h3 a.y-css-1x1e1r2');
@@ -15,22 +15,23 @@ window.startYelpScraper = async function() {
     link.click();
 
     // Wait for navigation
-    await new Promise(res => setTimeout(res, 2000 + Math.random()*1200));
+    await new Promise(res => setTimeout(res, 2000 + Math.random() * 1200));
 
-    // SCRAPE on business page
+    // SCRAPE on business page using only 100% confirmed selectors:
     let name = document.querySelector('h1')?.innerText || "";
     let address = document.querySelector('address')?.innerText || "";
-    let phone = "";
-    let phoneEl = Array.from(document.querySelectorAll('p')).find(p => /\(\d{3}\)\s?\d{3}-\d{4}/.test(p.innerText));
-    if(phoneEl) phone = phoneEl.innerText;
-    let reviews = "";
-    let reviewsEl = document.querySelector('[data-testid="reviewCount"]');
-    if(reviewsEl) reviews = reviewsEl.innerText;
-    results.push({name, address, phone, reviews});
+    let website = "";
+    let websiteEl = document.querySelector('a[href^="/biz_redir?url="]');
+    if (websiteEl) {
+      website = decodeURIComponent(websiteEl.href.split('url=')[1].split('&')[0]);
+    }
+    let phone = document.querySelector('p.y-css-qn4gww[data-font-weight="semibold"]')?.innerText || "";
+
+    results.push({name, address, website, phone});
 
     // Go back
     window.history.back();
-    await new Promise(res => setTimeout(res, 2200 + Math.random()*900));
+    await new Promise(res => setTimeout(res, 2200 + Math.random() * 900));
 
     // Re-select cards (in case page reloaded)
     businessCards = Array.from(document.querySelectorAll('div.toggle__09f24__fZMQ4[data-testid="scrollable-photos-card"]'));
